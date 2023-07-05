2915dfed7b3168bc0e584afd26d932c71aad5fd4
henryliu <17600047539@163.com>
2023-07-05 11:12:30 +0800

    update

---
+++ package/obelisk-client/src/index.ts
@@ -0,0 +1,10 @@
+export {
+  TransactionBlock,
+  SUI_CLOCK_OBJECT_ID,
+  SUI_SYSTEM_STATE_OBJECT_ID,
+} from '@mysten/sui.js';
+export { Obelisk } from './obelisk';
+export { SuiAccountManager } from './libs/suiAccountManager';
+export { SuiTxBlock } from './libs/suiTxBuilder';
+export { SuiRpcProvider } from './libs/suiRpcProvider';
+export type * from './types';
