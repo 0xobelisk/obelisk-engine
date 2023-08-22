123f5a484eeb0be253907abd8b63e9f0f6a3ffe6
henryliu <17600047539@163.com>
2023-08-22 17:23:30 +0800

    update

--- client/scripts/init1.ts
+++ client/scripts/init1.ts
@@ -1,13 +1,10 @@
 import { Obelisk } from '../src/obelisk';
 import * as process from 'process';
-import { NetworkType, ComponentContentType, SuiTxArgument } from '../src/types';
+import { NetworkType, SuiTxArgument } from '../src/types';
 import { TransactionBlock } from '@mysten/sui.js';
 import { initialize } from '../src/metadata/index';
 
 type DataItem = [number[], string];
-
-type DataType = 'string' | 'bool' | 'u64';
-
 function formatData(data: DataItem[]): string {
   const formattedData: string[] = [];
 
@@ -36,8 +33,10 @@ async function init() {
   const network = process.argv[2];
   const packageId = process.argv[3];
 
+  // Parse ABI
   const metadata = await initialize(network as NetworkType, packageId);
 
+  // new obelisk class
   const obelisk = new Obelisk({
     networkType: network as NetworkType,
     packageId: packageId,
@@ -46,6 +45,7 @@ async function init() {
   });
 
   const tx = new TransactionBlock();
+
   const params = [
     tx.pure(
       '0x6fa43c68221960f942572905f3c198a5bccaa0700506b3b6bd83dd9b007e6324'
@@ -55,6 +55,7 @@ async function init() {
     ) as SuiTxArgument,
     tx.pure('0x6') as SuiTxArgument,
   ] as SuiTxArgument[];
+
   const res1 = await obelisk.query.pet_system.get_pet_basic_info(tx, params);
   console.log(JSON.stringify(res1.results![0].returnValues));
 
@@ -64,44 +65,8 @@ async function init() {
     [[157, 136, 137, 14, 138, 1, 0, 0], 'u64'],
     [[11, 129, 14, 11, 0, 0, 0, 0], 'u64'],
   ];
-
   const formattedOutput: string = formatData(input);
   console.log(formattedOutput);
-  // console.log(obelisk.contractFactory.metadata)
-
-  // console.log(obelisk.contractFactory.metadata)
-
-  // let data = obelisk.contractFactory.getAllFunc();
-  // // console.log(data)
-
-  // console.log('---------- getWorld')
-  // let res = await obelisk.getWorld("0x17d08944903f2b9c0069872a895ee8a12c800e00c948e030138612e1e13cee4d")
-  // console.log(JSON.stringify(res))
-
-  // console.log('---------- getAllEntities')
-  // const worldId = "0x17d08944903f2b9c0069872a895ee8a12c800e00c948e030138612e1e13cee4d"
-  // let res1 = await obelisk.getAllEntities(worldId)
-  // // console.log(res1)
-  // res1.data.map((res) => {
-  //     console.log(res.name)
-  // })
-
-  // console.log('---------- getEntity')
-  // const entityId = "0xe881e7b52de6374ea3bb33c9b720102e9166786013f2d374395e12fce1d9a800"
-  // let res2 = await obelisk.getEntity(worldId, entityId)
-  // console.log(JSON.stringify(res2))
-
-  // console.log('---------- getEntityComponents')
-  // let comp = await obelisk.getEntityComponents(worldId, entityId);
-  // console.log(JSON.stringify(comp))
-
-  // console.log('---------- getBirthTime')
-  // const birthTimeId = "0x1b204289500b7e7a2c0d2f10838fd8471c587e8ecee24661652d1df18b061685"
-  // const sender = "0x1804b821bba181110599b8757008eabe6f89f62774d7fafb5ee666ac742a41f8"
-  // let time = await obelisk.getBirthTime(birthTimeId);
-  // console.log(JSON.stringify(time))
-
-  // target: `::status_system::get_pet_state`,
 }
 
 init();
