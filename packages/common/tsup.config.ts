d3dbfd3b4e33bdfca3667d0b39dc537208fa252c
henryliu <17600047539@163.com>
2023-09-06 20:45:06 +0800

    publish update

--- packages/common/tsup.config.ts
+++ packages/common/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/obelisk.ts"],
+  entry: ["src/index.ts"],
   target: "esnext",
   format: ["esm"],
   dts: false,
