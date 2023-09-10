192e12733594aaeef6d078576a910236561cd973
vladilen11 <vladilenaksana@163.com>
2023-09-10 23:20:38 +0800

    update

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -5,6 +5,7 @@ export {
 } from '@mysten/sui.js';
 export { Ed25519Keypair } from '@mysten/sui.js/keypairs/ed25519';
 export { fromB64, toB64 } from '@mysten/sui.js';
+export * from '@mysten/sui.js/bcs';
 export { Obelisk } from './obelisk';
 export { SuiAccountManager } from './libs/suiAccountManager';
 export { SuiTxBlock } from './libs/suiTxBuilder';
