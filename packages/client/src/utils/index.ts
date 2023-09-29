85eecbaf3f8cc99de056ea73b3ff8e7577ce4d47
vladilen11 <vladilenaksana@163.com>
2023-09-29 15:53:16 +0800

    Some nits.

--- packages/client/src/utils/index.ts
+++ packages/client/src/utils/index.ts
@@ -6,15 +6,12 @@ export function normalizeHexAddress(input: string): string | null {
   const hexRegex = /^(0x)?[0-9a-fA-F]{64}$/;
 
   if (hexRegex.test(input)) {
-    // 如果输入字符串以 "0x" 开头，直接返回
     if (input.startsWith('0x')) {
       return input;
     } else {
-      // 如果输入字符串不以 "0x" 开头，添加 "0x" 前缀并返回
       return '0x' + input;
     }
   } else {
-    // 如果输入字符串不是合法的 64 位十六进制地址，返回 null 或者抛出错误，具体取决于你的需求
     return null;
   }
 }
