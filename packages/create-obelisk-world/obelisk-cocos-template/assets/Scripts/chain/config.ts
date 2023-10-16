8e546e9ce07b9e79b03d73f9a55649eb9f29b21d
vladilen11 <vladilenaksana@163.com>
2023-10-16 21:01:19 +0800

    update cocos obelisk client

--- packages/create-obelisk-world/obelisk-cocos-template/assets/Scripts/chain/config.ts
+++ packages/create-obelisk-world/obelisk-cocos-template/assets/Scripts/chain/config.ts
@@ -2,8 +2,8 @@ type NetworkType = 'testnet' | 'mainnet' | 'devnet' | 'localnet';
 
 const NETWORK: NetworkType = 'localnet';
 
-const PACKAGE_ID = '0xa2688bba550044b9b3f6c9d6a62aaf743148a170a8bf5670131dd091c9174368'
-const WORLD_ID = '0x2bd08b20fb4096a0f44abcdba8d5b05285274889d034e6abb5e34f2f398ca867'
+const PACKAGE_ID = '0xb07232e8cbabf7c5e920210df0e77ed866e913c5cbfb39ec2d2b00447c50ca1d'
+const WORLD_ID = '0x86e3e2b09619b60b33947e64e9eddef2daed9944c6fa312778178ab2b311c05a'
 
 export {
     NETWORK,
