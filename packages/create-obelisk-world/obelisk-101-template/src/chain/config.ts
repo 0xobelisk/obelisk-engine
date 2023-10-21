bb165bf6b08b34c57152524d1484f28d4faf6b1e
vladilen11 <vladilenaksana@163.com>
2023-10-21 20:44:19 +0800

    update obelisk config

--- packages/create-obelisk-world/obelisk-101-template/src/chain/config.ts
+++ packages/create-obelisk-world/obelisk-101-template/src/chain/config.ts
@@ -2,8 +2,8 @@ type NetworkType = 'testnet' | 'mainnet' | 'devnet' | 'localnet';
 
 const NETWORK: NetworkType = 'localnet';
 
-const PACKAGE_ID = '0xa0822cd8581e2c41bd14aad4bfa2f052b1e232d5c174513f9220501cbab27f27'
-const WORLD_ID = '0xa3b0d7299bc816f545a2588c9cbd801024f5ea5a40da719598bf4662ff5264d7'
+const PACKAGE_ID = '0x43bd624fc804411011ce25f69b41854a38fb3e4fd87595fcbe86aa598ecedf9e'
+const WORLD_ID = '0xba0ed5680c90944dad19ec5053308820899b52025058ac3420655d72917ab9bf'
 
 export {
     NETWORK,
