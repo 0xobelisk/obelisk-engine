27c9482c8ccb25f361c4a42179beff2ce27812f5
vladilen11 <vladilenaksana@163.com>
2024-04-17 03:03:07 +0800

    export sui.js keypairs lib

--- packages/create-obelisk/template/101/sui-template/scripts/generateAccount.ts
+++ packages/create-obelisk/template/101/sui-template/scripts/generateAccount.ts
@@ -1,24 +1,33 @@
-import { Ed25519Keypair, fromB64 } from '@0xobelisk/sui-client';
+import { Ed25519Keypair, fromB64, encodeSuiPrivateKey } from '@0xobelisk/sui-client';
 import * as fs from 'fs';
+
+function uint8ArrayToHexString(uint8Array: Uint8Array): string {
+  return Array.from(uint8Array, byte => {
+    return ('0' + (byte & 0xff).toString(16)).slice(-2);
+  }).join('');
+}
 
 function generateAccount() {
   const keypair = new Ed25519Keypair();
 
-  const privateKey_u8 = fromB64(keypair.export().privateKey);
-  const privateKey = Buffer.from(privateKey_u8).toString('hex');
+  const privateKey_u8 = keypair.getSecretKey();
+  console.log(privateKey_u8);
+  // const privateKey = uint8ArrayToHexString(privateKey_u8);
+  // console.log(privateKey);
+  // const privateKey = Buffer.from(privateKey_u8).toString('hex');
 
-  const path = process.cwd();
-  const chainFolderPath = `${path}/src/chain`;
-  fs.mkdirSync(chainFolderPath, { recursive: true });
+  // const path = process.cwd();
+  // const chainFolderPath = `${path}/src/chain`;
+  // fs.mkdirSync(chainFolderPath, { recursive: true });
 
-  fs.writeFileSync(`${path}/.env`, `PRIVATE_KEY=${privateKey}`);
+  // fs.writeFileSync(`${path}/.env`, `PRIVATE_KEY=${privateKey}`);
 
-  fs.writeFileSync(
-    `${path}/src/chain/key.ts`,
-    `export const PRIVATEKEY = '${privateKey}';
-export const ACCOUNT = '${keypair.toSuiAddress()}';
-`,
-  );
+  //   fs.writeFileSync(
+  //     `${path}/src/chain/key.ts`,
+  //     `export const PRIVATEKEY = '${privateKey}';
+  // export const ACCOUNT = '${keypair.toSuiAddress()}';
+  // `,
+  //   );
 
   console.log(`Generate new Account: ${keypair.toSuiAddress()}`);
 }
