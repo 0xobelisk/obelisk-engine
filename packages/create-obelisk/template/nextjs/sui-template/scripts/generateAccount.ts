ec5f76d57998e557bca5a1f0e33ed3e932cb5dcd
vladilen11 <vladilenaksana@163.com>
2024-06-08 00:44:02 +0800

    upgrade sui-client: support suipriv key

--- packages/create-obelisk/template/nextjs/sui-template/scripts/generateAccount.ts
+++ packages/create-obelisk/template/nextjs/sui-template/scripts/generateAccount.ts
@@ -1,15 +1,21 @@
-import { Ed25519Keypair, fromB64 } from '@0xobelisk/sui-client';
+import { Ed25519Keypair } from '@0xobelisk/sui-client';
 import * as fs from 'fs';
 
 function generateAccount() {
   const keypair = new Ed25519Keypair();
-
-  const privateKey_u8 = fromB64(keypair.export().privateKey);
-  const privateKey = Buffer.from(privateKey_u8).toString('hex');
-
+  const privateKey = keypair.getSecretKey();
   const path = process.cwd();
+  const chainFolderPath = `${path}/src/chain`;
+  fs.mkdirSync(chainFolderPath, { recursive: true });
 
   fs.writeFileSync(`${path}/.env`, `PRIVATE_KEY=${privateKey}`);
+
+  fs.writeFileSync(
+    `${path}/src/chain/key.ts`,
+    `export const PRIVATEKEY = '${privateKey}';
+  export const ACCOUNT = '${keypair.toSuiAddress()}';
+  `,
+  );
 
   console.log(`Generate new Account: ${keypair.toSuiAddress()}`);
 }
