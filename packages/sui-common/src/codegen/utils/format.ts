68aa6d6f17d2ea2cedd41f7d04ce2b1e6972fb12
vladilen11 <vladilenaksana@163.com>
2024-10-28 09:35:51 +0800

    update sui cli and common version: update prettier-plugin-move-js import

--- packages/sui-common/src/codegen/utils/format.ts
+++ packages/sui-common/src/codegen/utils/format.ts
@@ -1,39 +1,39 @@
-import prettier from "prettier";
-import prettierPluginMove from "prettier-plugin-move";
+import prettier from 'prettier';
+import prettierPluginMove from 'prettier-plugin-move-js';
 
 export async function formatMove(
-  content: string,
-  prettierConfigPath?: string
+	content: string,
+	prettierConfigPath?: string
 ): Promise<string> {
-  let config;
-  if (prettierConfigPath) {
-    config = await prettier.resolveConfig(prettierConfigPath);
-  }
-  try {
-    return prettier.format(content, {
-      plugins: [prettierPluginMove],
-      parser: "move-parse",
-      printWidth: 120,
-      semi: true,
-      tabWidth: 2,
-      useTabs: false,
-      bracketSpacing: true,
-      ...config,
-    });
-  } catch (error) {
-    let message;
-    if (error instanceof Error) {
-      message = error.message;
-    } else {
-      message = error;
-    }
-    console.log(`Error during output formatting: ${message}`);
-    return content;
-  }
+	let config;
+	if (prettierConfigPath) {
+		config = await prettier.resolveConfig(prettierConfigPath);
+	}
+	try {
+		return prettier.format(content, {
+			plugins: [prettierPluginMove],
+			parser: 'move-parse',
+			printWidth: 120,
+			semi: true,
+			tabWidth: 2,
+			useTabs: false,
+			bracketSpacing: true,
+			...config,
+		});
+	} catch (error) {
+		let message;
+		if (error instanceof Error) {
+			message = error.message;
+		} else {
+			message = error;
+		}
+		console.log(`Error during output formatting: ${message}`);
+		return content;
+	}
 }
 
 export async function formatTypescript(content: string): Promise<string> {
-  return prettier.format(content, {
-    parser: "typescript",
-  });
+	return prettier.format(content, {
+		parser: 'typescript',
+	});
 }
--- packages/sui-common/tsup.config.ts
+++ packages/sui-common/tsup.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ["src/index.ts"],
-  target: "esnext",
-  format: ["esm"],
-  dts: false,
-  sourcemap: true,
-  clean: true,
-  minify: true,
+	entry: ['src/index.ts'],
+	target: 'esnext',
+	format: ['esm'],
+	dts: true, // 改为 true 以生成类型声明文件
+	sourcemap: true,
+	clean: true,
+	minify: true,
 });
