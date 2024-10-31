6b3a701e0b7bfe9417f887db4a04ab8e62680bc1
vladilen11 <vladilenaksana@163.com>
2024-10-31 23:53:09 +0800

    update create-obelisk: update cocos template

--- packages/create-obelisk/template/cocos/sui-template/contracts/counter/sources/script/deploy_hook.move
+++ packages/create-obelisk/template/cocos/sui-template/contracts/counter/sources/script/deploy_hook.move
@@ -1,19 +1,67 @@
-module counter::deploy_hook {
-    use counter::world::{World, AdminCap, get_admin};
-    use sui::object;
+#[allow(lint(share_owned))]
+  
+  module counter::deploy_hook {
 
-    /// Not the right admin for this world
-    const ENotAdmin: u64 = 0;
+  use obelisk::dapps_schema::Dapps;
 
-    public entry fun run(world: &mut World, admin_cap: &AdminCap) {
-        assert!( get_admin(world) == object::id(admin_cap), ENotAdmin);
-        
-        // Logic that needs to be automated once the contract is deployed
-        
-    }
+  use obelisk::dapps_system;
 
-    #[test_only]
-    public fun deploy_hook_for_testing(world: &mut World, admin_cap: &AdminCap){
-        run(world, admin_cap)
-    }
+  use counter::dapp_key::DappKey;
+
+  use std::ascii;
+
+  use sui::clock::Clock;
+
+  use sui::transfer::public_share_object;
+
+  #[test_only]
+
+  use obelisk::dapps_schema;
+
+  #[test_only]
+
+  use sui::clock;
+
+  #[test_only]
+
+  use sui::test_scenario;
+
+  #[test_only]
+
+  use sui::test_scenario::Scenario;
+
+  public entry fun run(dapps: &mut Dapps, clock: &Clock, ctx: &mut TxContext) {
+    // Register the dapp to obelisk.
+    dapps_system::register<DappKey>(
+            dapps,
+            ascii::string(b"counter"),
+            ascii::string(b"counter"),
+            clock,
+            ctx
+        );
+    let mut counter = counter::counter_schema::register(dapps, ctx);
+    // Logic that needs to be automated once the contract is deployed
+      counter.borrow_mut_value().set(0);
+
+    // Share the dapp object with the public
+    public_share_object(counter);
+  }
+
+  #[test_only]
+
+  public fun deploy_hook_for_testing(): (Scenario, Dapps) {
+    let mut scenario = test_scenario::begin(@0xA);
+    {
+          let ctx = test_scenario::ctx(&mut scenario);
+          dapps_schema::init_dapps_for_testing(ctx);
+          test_scenario::next_tx(&mut scenario,@0xA);
+      };
+    let mut dapps = test_scenario::take_shared<Dapps>(&scenario);
+    let ctx = test_scenario::ctx(&mut scenario);
+    let clock = clock::create_for_testing(ctx);
+    run(&mut dapps, &clock, ctx);
+    clock::destroy_for_testing(clock);
+    test_scenario::next_tx(&mut scenario,@0xA);
+    (scenario, dapps)
+  }
 }
--- packages/create-obelisk/template/cocos/sui-template/scripts/storeConfig.ts
+++ packages/create-obelisk/template/cocos/sui-template/scripts/storeConfig.ts
@@ -1,68 +1,75 @@
-import * as fsAsync from "fs/promises";
-import { mkdirSync, writeFileSync } from "fs";
-import { exit } from "process";
-import { obeliskConfig } from "../assets/Scripts/obelisk.config";
-import { dirname } from "path";
+import * as fsAsync from 'fs/promises';
+import { mkdirSync, writeFileSync } from 'fs';
+import { exit } from 'process';
+import { obeliskConfig } from '../assets/Scripts/obelisk.config';
+import { dirname } from 'path';
+
+export type schema = {
+	name: string;
+	objectId: string;
+};
 
 type DeploymentJsonType = {
-  projectName: string;
-  network: "mainnet" | "testnet" | "devnet" | "localnet";
-  packageId: string;
-  worldId: string;
-  upgradeCap: string;
-  version: number;
+	projectName: string;
+	network: 'mainnet' | 'testnet' | 'devnet' | 'localnet';
+	packageId: string;
+	schemas: schema[];
+	upgradeCap: string;
+	version: number;
 };
 
 async function getDeploymentJson(projectPath: string, network: string) {
-  try {
-    const data = await fsAsync.readFile(
-      `${projectPath}/.history/sui_${network}/latest.json`,
-      "utf8"
-    );
-    return JSON.parse(data) as DeploymentJsonType;
-  } catch {
-    console.log("store config failed.");
-    exit;
-  }
+	try {
+		const data = await fsAsync.readFile(
+			`${projectPath}/.history/sui_${network}/latest.json`,
+			'utf8'
+		);
+		return JSON.parse(data) as DeploymentJsonType;
+	} catch {
+		console.log('store config failed.');
+		exit;
+	}
 }
 
-function storeConfig(network: string, packageId: string, worldId: string) {
-  let code = `type NetworkType = 'testnet' | 'mainnet' | 'devnet' | 'localnet';
+function storeConfig(network: string, packageId: string, schemas: schema[]) {
+	let code = `type NetworkType = 'testnet' | 'mainnet' | 'devnet' | 'localnet';
 
-const NETWORK: NetworkType = '${network}';
+export const NETWORK: NetworkType = '${network}';
 
-const PACKAGE_ID = '${packageId}'
-const WORLD_ID = '${worldId}'
+export const PACKAGE_ID = '${packageId}'
 
-export {
-    NETWORK,
-    PACKAGE_ID,
-    WORLD_ID,
-}
+${schemas
+	.map(
+		schema =>
+			`export const ${schema.name.split('::')[2]}_Object_Id = '${
+				schema.objectId
+			}'`
+	)
+	.join('\n')}
 `;
-  const path = process.cwd();
-  writeOutput(code, `${path}/assets/Scripts/chain/config.ts`, "storeConfig");
+	const path = process.cwd();
+	writeOutput(code, `${path}/assets/Scripts/chain/config.ts`, 'storeConfig');
 }
 
 async function writeOutput(
-  output: string,
-  fullOutputPath: string,
-  logPrefix?: string
+	output: string,
+	fullOutputPath: string,
+	logPrefix?: string
 ): Promise<void> {
-  mkdirSync(dirname(fullOutputPath), { recursive: true });
+	mkdirSync(dirname(fullOutputPath), { recursive: true });
 
-  writeFileSync(fullOutputPath, output);
-  if (logPrefix !== undefined) {
-    console.log(`${logPrefix}: ${fullOutputPath}`);
-  }
+	writeFileSync(fullOutputPath, output);
+	if (logPrefix !== undefined) {
+		console.log(`${logPrefix}: ${fullOutputPath}`);
+	}
 }
 
 async function main() {
-  const path = process.cwd();
-  const network = process.argv[2];
-  const contractPath = `${path}/contracts/${obeliskConfig.name}`;
-  const deployment = await getDeploymentJson(contractPath, network);
-  storeConfig(deployment.network, deployment.packageId, deployment.worldId);
+	const path = process.cwd();
+	const network = process.argv[2];
+	const contractPath = `${path}/contracts/${obeliskConfig.name}`;
+	const deployment = await getDeploymentJson(contractPath, network);
+	storeConfig(deployment.network, deployment.packageId, deployment.schemas);
 }
 
 main();
